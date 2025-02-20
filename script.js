48e32fd98828f3240d6548620591b8c4f16370f6
Nishar24 <mdnishar75640@gmail.com>
2025-02-21 00:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
+// Car Constructor Function
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Adding method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// SportsCar Constructor Function (Inheriting from Car)
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model); // Call Car constructor with this context
+    this.topSpeed = topSpeed;
+}
+
+// Inherit prototype from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Adding method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
